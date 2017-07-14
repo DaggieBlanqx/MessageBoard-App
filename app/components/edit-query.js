5cdb4695b17e8a48254bfb3dd456b66784bd73b5
sammwas <samson.m.njuguna@gmail.com>
2017-07-14 18:20:10 +0300

    you can now update your question

---
+++ app/components/edit-query.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+   editForm: false,
+
+     actions :{
+       updateQuestion(){
+         this.set('editForm',true)
+       },
+       edit(board){
+         var params={
+         author: this.get('author'),
+         question: this.get('question'),
+         description: this.get('description')
+         };
+         this.set('editForm',false);
+         this.sendAction('editQuery',params,board);
+       }
+   }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -13,6 +13,15 @@ export default Ember.Route.extend({
       var nwQuestion=this.store.createRecord('board',params);
       nwQuestion.save();
       this.transitionTo('index');
+    },
+    editTile(board, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          board.set(key,params[key]);
+        }
+      });
+      board.save();
+      this.transitionTo('index');
     }
   }
 });
